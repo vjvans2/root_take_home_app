ddec6718c30728bd5df561d09c73915124fb8fd8
Vincent VanSlyke <vincent.vanslyke@gmail.com>
2021-05-21 00:31:48 -0400

    got the baseline tests and logic working.  Now to add more tests and crunchy bits.

--- lib/driving_history.rb
+++ lib/driving_history.rb
@@ -1,13 +1,15 @@
 class DrivingHistory
-  FILE_REGEX = '^[A-Za-z0-9\/\_\ ]+\.txt$'
+  FILE_REGEX = '^[A-Za-z0-9\/\_\ ]+\.txt$' # any combo of directories, text, underscores, spaces as long as it has a '.txt' at the end'
 
-  attr_accessor :filepath, :file_data
+  attr_accessor :filepath, :file_data, :driver_trips_array
 
   InvalidFileError = Class.new(StandardError)
   InvalidCommandError = Class.new(StandardError)
 
   def initialize(filepath)
     @filepath = filepath
+    @file_data = File.open(filepath).readlines.map(&:chomp)
+    @driver_trips_array = []
   end
 
   def self.report(filepath)
@@ -15,87 +17,111 @@ class DrivingHistory
   end
 
   def report
-    'hi dood'
-    # catch and rescue errors for performance
-    # file_valid?
+    # catch and rescue errors for performance?
+    file_valid?
     
-    # output_report    
-
+    output_report    
   end
 
   private
 
-  def file_data
-    @file_data = File.open(filepath)&.readlines.map(&:chomp)
-  end
+    # def driver_trips_by_name(name) # when I use this method it is an "undefined method" - why?
+  #   driver_trips_array.find { |dt| dt.key?(name) }
+  # end
 
-  def file_valid?
-    # is the filepath a type .txt?  - this may be best got with a regex to check the last four char
-    raise InvalidFileError, 'file is not a .txt' if filepath.match(FILE_REGEX).nil?
-    match = filepath.match(FILE_REGEX).nil?
-
-    # is the file populated?
-    raise InvalidFileError, 'file is not populated' if file_data.empty?
-
-    # is it delimited correctly?  Should we try to perceive it? 2.0?
-  end
-
-  def output_report
-    generate_output(driver_trips)
-  end
-
-  def driver_trips
-    dts = [] # make me class level? I need the methods to be able to add something to the array
-    # or I can make it more like driver_trips << driver(row)
-
-    file_data.each do |row|
-      split = row.split(' ')
-      if split[0] == 'Driver'
-        #driver things
-      elsif split[0] == 'Trip'
-        #trip things
-      else
-        raise InvalidCommandError, "The provided #{split[0]} is invalid for this app."
-    end
-  end
-  
-  # # DO NOT FORGET TO ACCOUNT FOR MARY SUE (names with spaces)
-
-  def driver(row)
-    # check to see if the name (split[1]) exists in the array of objects
-    # if it does, next
-    # if it doesnt - make an object: {driver: 'name', trips: []}
-  end
-
-  def trip(row)
-    # check to see if the name (split[1]) exists in the array of objects
-    # if it does, add it to the trip array
-    # if it doesnt - make an object: {driver: 'name', trips: [current_trip]}
+  def trip_instance(split, single_word_name = true)
+    {
+      start: split[single_word_name ? 2 : 3], 
+      end: split[single_word_name ? 3 : 4],
+      miles: split[single_word_name ? 4 : 5],
+    }
   end
 
   def generate_output(driver_trips)
     output = ''
     driver_trips.each do |dt|
-      trip_data = calc_trip_data(dt[:trips])
-      driver_data_string = "#{dt[:name]}: #{trip_data}\n"
-
-      output += driver_data_string
+      dt[:miles] = total_miles_calc(dt[:trips])
+      dt[:mph] = mph_calc(dt[:trips])
     end
+
+    driver_trips.sort_by { |dt| dt[:miles] }.reverse.each do |x|
+      output += "#{x[:name]}: #{x[:miles]} miles"
+
+      if x[:miles] == 0
+        output += "\n"
+        next
+      end
+
+      output += " @ #{x[:mph]} mph\n"
+    end
+
+    output.strip # the strip removes the final \n
   end
 
-  def calc_trip_data(trips)
-    return '0 miles' if trips.empty?
-    
-    miles = miles_calc(trips)
-    mph = mph_calc(trips)
+  def total_miles_calc(trips)
+    return 0 if trips.empty?
 
-    "#{miles} miles @ #{mph} mph"
-  end
-
-  def miles_calc(trips)
+    miles = 0
+    trips.each { |t| miles += t[:miles].to_f.round } # going "to_i" always rounds down, round with no params handles the int-ness
+    miles
   end
 
   def mph_calc(trips)
+    return 0 if trips.empty?
+
+    require 'time'
+    mphs = []
+    trips.each do |t|
+      start = Time.parse(t[:start])
+      finish = Time.parse(t[:end])
+      miles = t[:miles].to_f.round
+      hour_diff = (finish-start)/3600 # in seconds, /60 to get to minutes, /60 to get to hours
+      mphs << miles/hour_diff
+    end
+
+    (mphs.sum / mphs.length).round
   end
-end
+
+  def file_valid?
+    # is the filepath a type .txt?  - this may be best got with a regex to check the last four char
+    raise InvalidFileError, 'file is not a .txt' if filepath.match(FILE_REGEX).nil?
+
+    # is the file populated?
+    raise InvalidFileError, 'file is not populated' if file_data.empty?
+
+    # is it delimited correctly?  Should we try to perceive it? 2.0? (could set up a delimiter as assumed parameter pending override)
+  end
+
+  def output_report
+    dt = get_driver_trips
+    generate_output(dt)
+  end
+
+  def get_driver_trips
+    file_data.each do |row|
+      next if row.empty?
+      
+      split = row.split(' ')
+
+      if split[0] == 'Driver'
+        name = split[1] # MARY SUE if length > 2
+        if driver_trips_array.empty? || driver_trips_array.select { |dt| dt[:name] == name }.empty?
+            driver_trips_array <<  {name: name, trips: [] }
+        end
+      elsif split[0] == 'Trip'
+        name = split[1] # MARY SUE if length > 5
+        # single_word_name = split.length != EXPECTED_TRIP_LENGTH && Time.parse(split[2]).nil?
+        dt_by_name = driver_trips_array.select { |dta| dta[:name] == name }.first
+
+        if driver_trips_array.empty? || dt_by_name.nil?
+          driver_trips_array <<  {name: name, trips: [trip_instance(split)] }
+        end
+        dt_by_name[:trips] << trip_instance(split)
+      else
+        raise InvalidCommandError, "The provided #{split[0]} is invalid for this app."
+      end
+
+    end
+    driver_trips_array
+  end
 end
--- spec/driving_history_spec.rb
+++ spec/driving_history_spec.rb
@@ -2,19 +2,45 @@
 
 # run me with 'bundle exec rspec'
 
-require "driving_history"
+require 'driving_history'
 
 RSpec.describe DrivingHistory do
-  subject { described_class.report(file)}
+  subject { described_class.report(filename) }
 
-  let(:file) { File.write('test_data.txt', file_contents) }
+  let(:filename) { 'test_data.txt' }
   let(:file_contents) { "Driver A\nDriver B\nDriver C\nTrip A 07:15 07:45 15.9345\nTrip A 09:15 09:56 29.3\nTrip B 14:11 15:42 39.5" }
+  let(:file) { File.write(filename, file_contents) }
+
+  before do
+    file
+  end
 
   describe '#report' do
-    context 'when the file is provided' do
-      it 'returns "hi dood" because that\'s all I\'ve included' do
-        expect(subject).to eq 'hi dood'
+    # HAPPY PATHS
+    context 'when a valid file is provided - 1:2, 1:1, 1:0 - provided order' do
+      let(:expected_report) { "A: 45 miles @ 37 mph\nB: 40 miles @ 26 mph\nC: 0 miles" }
+      it 'returns the intended result' do
+        expect(subject).to eq expected_report
       end
     end
+
+    context 'when a valid file is provided - 1:2, 1:1, 1:0 - out of order' do
+      let(:file_contents) { "Driver A\nTrip A 07:15 07:45 15.9345\nDriver B\nTrip A 09:15 09:56 29.3\nTrip B 14:11 15:42 39.5\nDriver C" }
+      let(:expected_report) { "A: 45 miles @ 37 mph\nB: 40 miles @ 26 mph\nC: 0 miles" }
+      it 'returns the intended result' do
+        expect(subject).to eq expected_report
+      end
+    end
+
+    context 'when a valid file is provided - 1:5, 1:0, 1:0 - out of order' do
+      let(:file_contents) { "Driver A\nTrip A 07:15 07:45 15.9345\nDriver B\nTrip A 09:15 09:56 29.3\nTrip A 14:11 15:42 39.5\nDriver C\nTrip A 11:15 14:56 89.3\nTrip A 19:15 21:56 59.3" }
+      let(:expected_report) { "A: 233 miles @ 29 mph\nC: 0 miles\nB: 0 miles" }
+      it 'returns the intended result' do
+        expect(subject).to eq expected_report
+      end
+    end
+
+    # add a test for empty rows lines within the file
+
   end
 end
